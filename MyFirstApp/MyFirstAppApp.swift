0d6f4704ca536246a7d0587d57166fa6ef7fd2a7
YEJI, MOON <yeahg.dev@gmail.com>
2021-07-02 15:03:18 +0900

    First Xcode commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by 1 on 2021/07/02.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
